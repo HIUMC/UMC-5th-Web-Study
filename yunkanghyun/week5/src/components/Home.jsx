d4928bb4db6ac4e71dbd62f42ee7912687ad2782
Andy Yun <andy000923@gmail.com>
2023-10-31 03:06:25 +0900

    5주차 과제 템플릿

---
+++ yunkanghyun/week5/src/components/Home.jsx
@@ -0,0 +1,10 @@
+import Untitled from "../Untitled.svg";
+function Home() {
+    return (
+      <div>
+        <img style={{ width: "100%" }} src={Untitled} alt="home" />
+      </div>
+    );
+  }
+  
+  export default Home;
