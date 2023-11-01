e73856628e926ff64a4f18cf5894cde1fb94306e
Andy Yun <andy000923@gmail.com>
2023-11-01 18:46:33 +0900

    이벤트핸들러 연습

--- yunkanghyun/week5/src/components/TVprogram.jsx
+++ yunkanghyun/week5/src/components/TVprogram.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 export default function TVprogram() {
     return (
-        <div>
+        <div style={{color:"white"}}>
             이것은 TV 프로그램 페이지 입니다
         </div>
     );
--- yunkanghyun/week5/src/routes/index.js
+++ yunkanghyun/week5/src/routes/index.js
@@ -1,5 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Movie from "../components/MovieList.jsx";
+import TV from "../components/TVprogram.jsx";
+import Person from "../components/Person.jsx";
 import Header from "../components/Header.jsx";
 import Home from "../components/Home.jsx";
 const RootRoute = () => {
@@ -10,6 +12,8 @@ const RootRoute = () => {
             <Routes>
                 <Route path='/' element={<Home/>}/>
                 <Route path='/movie' element={<Movie/>} />
+                <Route path='/tv' element={<TV/>} />
+                <Route path='/person' element={<Person/>} />
             </Routes>
         </BrowserRouter>
     )
