b3ae66ffd10f20a8a63fee4f81b4a8e18a0747c6
Andy Yun <andy000923@gmail.com>
2023-11-07 20:50:44 +0900

    6주차 실습 빈칸

--- yunkanghyun/week6/src/components/MovieDetail.jsx
+++ yunkanghyun/week6/src/components/MovieDetail.jsx
@@ -5,7 +5,10 @@ const MovieDetail = () => {
 
   const location = useLocation();
 
-
+    //이부분에 코드 작성. 코드 테스트 할때는 아래 주석 풀고 실행해보세요.
+    //아래의 주석처리된 부분의 movieTitle과 poster_path를 파라미터를 통해 가져와야 함.
+    //코드는 두줄이면 됨. 쿼리를 어떻게 가져오는지에 대한 중요한 실습이니 꼭 생각 해보기.
+    //정답은 내일 스터디시간에 공개하겠습니다.
 
   return (
     <div>
